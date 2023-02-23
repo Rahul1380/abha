db02e994944fad44dc42333c95184092dbe607f2
rahul sawhney <you@example.com>
2023-02-23 12:52:48 +0530

    2nd commit

--- abha/src/main/java/com/abym/abha/Constants/AppConstants.java
+++ abha/src/main/java/com/abym/abha/Constants/AppConstants.java
@@ -6,6 +6,6 @@ public class AppConstants {
     public static String CLIENT_SECRET = "";
     public static boolean IS_VERIFIED = false;
     public static String APPMODE = "mode";
-    public static String UAT = "development";
     public static String PROD = "production";
+    public static String UAT = "development";
 }
