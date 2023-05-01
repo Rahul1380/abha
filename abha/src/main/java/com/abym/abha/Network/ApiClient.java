5b07a3637a02a2b9e451b9ba9eba2b46a415a6a7
rahul sawhney <you@example.com>
2023-05-01 17:26:53 +0530

    updated code

--- abha/src/main/java/com/abym/abha/Network/ApiClient.java
+++ abha/src/main/java/com/abym/abha/Network/ApiClient.java
@@ -44,63 +44,6 @@ public class ApiClient {
         return retrofit;
     }
 
-    public static Retrofit getApiClient1(Context mContext, String BASE_URL) {
-        context = mContext;
-        Retrofit retrofit = null;
-        if (retrofit == null) {
-            CookieManager cookieManager = new CookieManager();
-            cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
-            HashMap<String, String> headers = new HashMap<>();
-
-            OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder().addInterceptor(new HeaderIntercepter1());
-            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-            clientBuilder.addInterceptor(new NetworkConnectionInterceptor(mContext));
-            clientBuilder.addInterceptor(loggingInterceptor);
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .client(clientBuilder.build())
-                    .build();
-        }
-        return retrofit;
-    }
-
-    public static Retrofit getApiClient2(Context mContext, String BASE_URL) {
-        context = mContext;
-        Retrofit retrofit = null;
-        if (retrofit == null) {
-            CookieManager cookieManager = new CookieManager();
-            cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
-            HashMap<String, String> headers = new HashMap<>();
-
-            OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder().addInterceptor(new HeaderIntercepter2());
-            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-            clientBuilder.addInterceptor(new NetworkConnectionInterceptor(mContext));
-            clientBuilder.addInterceptor(loggingInterceptor);
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .client(clientBuilder.build())
-                    .build();
-        }
-        return retrofit;
-    }
-
-    public static class HeaderIntercepter1 implements Interceptor {
-        public Response intercept(Interceptor.Chain chain) throws IOException {
-            Request.Builder addHeader = chain.request().newBuilder().addHeader("content", "application/json").addHeader("Accept-Language", "en-US");
-            return chain.proceed(addHeader.addHeader("Authorization", "Bearer " + PreferenceUtil.getStringPrefs(ApiClient.context, PreferenceUtil.HEALTH_ACCESSTOKEN, "")).build());
-        }
-    }
-
-    public static class HeaderIntercepter2 implements Interceptor {
-        public Response intercept(Interceptor.Chain chain) throws IOException {
-            return chain.proceed(chain.request().newBuilder().addHeader("Content-Type", "application/json").addHeader("X-AUTH-TOKEN", PreferenceUtil.getStringPrefs(ApiClient.context, PreferenceUtil.XUSERTOKEN_PHR, "")).build());
-        }
-    }
-
     public static class HeaderIntercepter implements Interceptor {
 
         public Response intercept(Interceptor.Chain chain) throws IOException {
